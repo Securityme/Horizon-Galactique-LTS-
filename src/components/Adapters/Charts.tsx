9a22249ee031446dd0df47542d134a9659bd0d2a
Securityme <120278936+Securityme@users.noreply.github.com>
2026-08-31 11:59:12 +0200

    feat: add Adapters and Resilience/Canvas2D component placeholders

---
+++ src/components/Adapters/Charts.tsx
@@ -0,0 +1,11 @@
+/**
+ * STR-01.01.06.03 - Adaptateur charts
+ * Adapters Charts component
+ * @status TODO
+ * @priority P1
+ */
+export const AdaptersCharts: React.FC<{
+  data: number[];
+}> = ({ data }) => {
+  return <div className="adapters-charts">Adapter Chart</div>;
+};
